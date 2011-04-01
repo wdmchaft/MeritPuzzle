67753e5a768a5b043627b8aebb669ed1b06c51d7
Saad Shams <saadshams@hotmail.com>
2011-04-01 17:13:42 +0400

    Initial Commit

---
+++ MeritPuzzle/MeritPuzzleAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  MeritPuzzleAppDelegate.h
+//  MeritPuzzle
+//
+//  Created by Saad Shams on 4/1/11.
+//  Copyright 2011 Leo Burnett. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MeritPuzzleAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
